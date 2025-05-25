17900c9575530e616202733b5d0a56cb10a52d90
monkeyluffy824 <138200438+monkeyluffy824@users.noreply.github.com>
2025-05-25 22:05:40 +0530

    Added new buttons

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 function viewFile() {
   window.open("files/panangipallisaicharanmaycv.pdf", "_blank");
 }
+
+function viewApex(){
+	window.open("https://panangipalli-sai-charan-p-dev-ed.develop.my.site.com/jsontoapex", "_blank");
+}
+
+function viewPackage(){
+	window.open("https://login.salesforce.com/packaging/installPackage.apexp?p0=04tdL0000006yBB", "_blank");
+}
